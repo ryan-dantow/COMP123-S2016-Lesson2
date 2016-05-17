5c8a3307a6253bfd784f8708c3f14f111cea7df9
ryan-dantow <rdantow@gmail.com>
2016-05-17 12:55:23 -0400

    Added a reference driver class from UnitTest

--- COMP123-S2016-Lesson2/Program.cs
+++ COMP123-S2016-Lesson2/Program.cs
@@ -9,13 +9,26 @@ using System.Threading.Tasks;
  * Date: May 17th 2016
  * Date Modified: may 17 2016
  * Description: Advanced Methods Demo for Lesson 2
- * Version: 0.0.1 - Initial Version
+ * Version: 0.0.2 - Added a reference driver class from UnitTest
  */
+
+    
  
 namespace COMP123_S2016_Lesson2
 {
+    /**
+     * Main Driver
+     * 
+     * @class Program
+     */
     class Program
     {
+        /**
+         * Main Method for our Driver Class
+         * 
+         * @constructor Main
+         * @param {string[]} args
+         */ 
         static void Main(string[] args)
         {
         }
--- UnitTestForLesson2/UnitTest1.cs
+++ UnitTestForLesson2/UnitTest1.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using COMP123_S2016_Lesson2; 
 namespace UnitTestForLesson2
 {
-    class Program
+    class UnitTest1
     {
         static void Main(string[] args)
         {
+            // Arrange
+
+            // Act
+
+            // Assert
         }
     }
 }
