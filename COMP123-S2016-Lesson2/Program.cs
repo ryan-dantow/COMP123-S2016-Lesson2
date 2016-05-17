41e540186e6696e691fc5671c54865c7981d7d13
ryan-dantow <rdantow@gmail.com>
2016-05-17 12:43:23 -0400

    Initial Version 0.0.1 Commit

--- COMP123-S2016-Lesson2/Program.cs
+++ COMP123-S2016-Lesson2/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Author: Ryan Dantow
+ * Date: May 17th 2016
+ * Date Modified: may 17 2016
+ * Description: Advanced Methods Demo for Lesson 2
+ * Version: 0.0.1 - Initial Version
+ */
+ 
 namespace COMP123_S2016_Lesson2
 {
     class Program
